9f72309912b488b7909206cc3f37f14e7921f782
Azidzainuri <zainuriazid@gmail.com>
2020-07-21 17:18:09 +0700

    Create province.php

---
+++ province.php
@@ -0,0 +1,30 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+$url = "https://api.rajaongkir.com/starter/province" . ((isset($_GET['id'])) ? "?id=" . $_GET['id'] : "");
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => array(
+        "key: af3addd9df5f1154d101824312efb522"
+    ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+    echo "cURL Error #:" . $err;
+} else {
+    echo $response;
+}
